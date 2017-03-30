574af82e56eb6d2ce4502eb36ea2e62bcf63ff79
rendawei123 <lovedavidren@outlook.com>
2017-03-30 17:39:42 +0800

    add a file about the Newton method about sqrt

---
+++ Sqrt.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        result = x
+
+        while result ** 2 - x > 0:
+            result = (result + x / result) / 2
+
+        return result
+
+if __name__ == '__main__':
+    x = 4
+    a = Solution()
+    print(a.mySqrt(x))
