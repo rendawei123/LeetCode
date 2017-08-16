6a5db919b759b4b8407013b574f14731f4cb6023
rendawei123 <lovedavidren@outlook.com>
2017-08-16 19:46:35 +0800

    add a new solution about two sum

--- TwoSum.py
+++ TwoSum.py
@@ -1,12 +1,10 @@
 # Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
 # 题目要求：给出一个整数数组nums，和一个数target,假设数组里面有且只有两个数的和为target，求这两个数在数组中的索引,要求不能重复使用一个元素
-# 这种做法是比较lowb的方法，让k等于列表末尾的一个数，用第一个数分别和后面所有
-# 的数比较，看时候符合题意，然后用第二个数和后面所有的数比较，如果符合题意则输出序号
-# 这种方法超出了时间，效率比较慢
 
 
 class Solution(object):
+    # lowB法，两重循环挨个判断，效率很低
     def twoSum(self, nums, target):
         i = 0
         j = 1
@@ -27,6 +25,20 @@ class Solution(object):
             return result
         else:
             return None
+
+    # 由于只有唯一解，可以考虑利用字典的特性，两边扫描求解，第一遍扫描数组，将
+    # 所有的数存放于一个字典中，字典中的建为数组的元素，值为元素在数组中的索引
+    # 第二遍扫描数组,看数组中的每个元素num[i]看target-num[i]是否在字典中,若在，输出结果
+    def two_sum_2(self, nums, target):
+        d = {}
+        for i in range(len(nums)):
+            if target-nums[i] in d:
+                return [d[target-nums[i]], i]
+            else:
+                d[nums[i]] = i
+
+    # 第三种解法
+
 
 if __name__ == '__main__':
     nums = [3, 2, 3]
