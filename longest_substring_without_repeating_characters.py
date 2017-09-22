a08f8d260afab3e91c417f8edcf21f67f083702c
rendawei123 <lovedavidren@outlook.com>
2017-09-22 20:22:01 +0800

    add an better sulution about this

--- longest_substring_without_repeating_characters.py
+++ longest_substring_without_repeating_characters.py
@@ -17,25 +17,50 @@ Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer 
 每次遍历一遍之后都记录一次最大字符长度
 然后判断字典里的字符个数，如果大于1，则表示有重复，start+1，否则也就是左边窗口向右滑动，这样，遍历完成之后
 返回最大字符长度
+
+二、第二种方法相比较第一种方法来说更近了一步，由于只需要求出最长的子串的长度，所以只需要一个用于跳转的指针start，和一个计数的变量maxlength即可，还需要一个用来判断是否重复的字典。
+让start初次为零
+遍历字符串，将第一次出现的字母以 key=s[i]:value=i 的形式存储在字典中，
+每遍历一个字母，判断这个字母是否在字典中，如果不在，添加进去，如果在，说明遇到了重复字符，不符合题意，将start指针跳跃到当前循环的字符，重新开始
 """
 
+
 class Solution(object):
+    """
+    在一个字符串中找到一个最长的子串，前提是子串中没有重复的字符,返回子串长度
+    """
     def lengthOfLongestSubstring(self, s):
         """
         :type s: str
         :rtype: int
         """
-        max_count, start, end = 0, 0, 0
-        count_dict = {}
-        for i in s:
-            end += 1
-            count_dict[i] = count_dict.get(i, 0) + 1
-            while count_dict[i] > 1:
+        # 滑动窗口法
 
-                count_dict[s[start]] -= 1
-                start += 1
-            max_count = max(max_count,(end - start))
-        return max_count
+        # max_count, start, end = 0, 0, 0
+        # count_dict = {}
+        # for i in s:
+        #     end += 1
+        #     count_dict[i] = count_dict.get(i, 0) + 1
+        #     while count_dict[i] > 1:
+        #
+        #         count_dict[s[start]] -= 1
+        #         start += 1
+        #     max_count = max(max_count,(end - start))
+        # return max_count
+
+        # 第二种方法，跳跃法
+        max_length = start = 0
+        usedChar = {}
+
+        for i in range(len(s)):
+            if s[i] in usedChar and start <= usedChar[s[i]]:
+                start = usedChar[s[i]] + 1
+            else:
+                max_length =max(max_length, ( i - start + 1))
+
+            usedChar[s[i]] = i
+        return max_length
+
 
 
 if __name__ == '__main__':
