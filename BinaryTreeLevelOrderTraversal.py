da78ebc0bab2f01c783fcc93bf86291b0707c5cb
rendawei123 <lovedavidren@outlook.com>
2017-10-14 09:50:27 +0800

    add annotation

--- BinaryTreeLevelOrderTraversal.py
+++ BinaryTreeLevelOrderTraversal.py
@@ -1,3 +1,29 @@
+"""
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its bottom-up level order traversal as:
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+
+题意：
+遍历二叉树
+
+解法：
+使用回溯法遍历，
+使用栈来作为缓存机构
+采用深度优先法
+"""
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
@@ -26,4 +52,3 @@ class Solution(object):
                 stack.append((node.left, level+1))
 
         return result
-
