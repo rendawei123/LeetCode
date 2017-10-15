12e6cdee916113cf1c8706a639c5f815158fcff8
rendawei123 <lovedavidren@outlook.com>
2017-10-15 11:55:08 +0800

    annotation

--- Implement_strStr-1.py
+++ Implement_strStr-1.py
@@ -1,3 +1,17 @@
+"""
+Implement strStr().
+
+Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+字串匹配
+返回字串在字符串中的索引，如果没有的化返回-1
+
+解法：
+由于只要求匹配出第一次出现的索引，因此只要找到就可以
+直接循环一次进行匹配
+"""
+
+
 class Solution(object):
     def strStr(self, haystack, needle):
         """
